b6bfbf3e20f98f82c9b60bfbf06af4735d107332
Toni Petrina <tonipetrina@hotmail.com>
2014-12-04 23:23:09 +0100

    Implemented IfNullUseParameter aka ?? converter

--- MassivePixel.Common.WP8/Converters/IfNullUseParameter.cs
+++ MassivePixel.Common.WP8/Converters/IfNullUseParameter.cs
@@ -1,6 +1,19 @@
-﻿namespace MassivePixel.Common.Converters
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MassivePixel.Common.Converters
 {
-    public class IfNullUseParameter
+    public class IfNullUseParameter : IValueConverter
     {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value ?? parameter;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
