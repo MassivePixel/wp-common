bc1d2e9b5567b3d0b9e79d8056a5ce3d0a77949b
Toni Petrina <tonipetrina@hotmail.com>
2014-12-04 23:34:22 +0100

    Added EnumToValue converter

---
+++ MassivePixel.Common.WP8/Converters/EnumToValue.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace MassivePixel.Common.Converters
+{
+    [ContentProperty("Mappings")]
+    public class EnumToValue : DependencyObject, IValueConverter
+    {
+        #region Mappings
+        public static readonly DependencyProperty MappingsProperty = DependencyProperty.Register(
+            "Mappings", typeof(List<EnumValueMapping>), typeof(EnumToValue), new PropertyMetadata(new List<EnumValueMapping>()));
+
+        public List<EnumValueMapping> Mappings
+        {
+            get { return (List<EnumValueMapping>)GetValue(MappingsProperty); }
+            set { SetValue(MappingsProperty, value); }
+        }
+        #endregion
+
+        #region DefaultReturnValue
+        public static readonly DependencyProperty DefaultReturnValueProperty = DependencyProperty.Register(
+            "DefaultReturnValue", typeof(object), typeof(EnumToValue), new PropertyMetadata(default(object)));
+
+        public object DefaultReturnValue
+        {
+            get { return GetValue(DefaultReturnValueProperty); }
+            set { SetValue(DefaultReturnValueProperty, value); }
+        }
+        #endregion
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return DefaultReturnValue;
+
+            foreach (var mapping in Mappings)
+            {
+                if (mapping.Enum == value.ToString())
+                    return mapping.Value;
+            }
+
+            return DefaultReturnValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MassivePixel.Common.WP8/Converters/EnumValueMapping.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Markup;
+
+namespace MassivePixel.Common.Converters
+{
+    [ContentProperty("Value")]
+    public class EnumValueMapping : DependencyObject
+    {
+        #region Enum dependency property
+        public static readonly DependencyProperty EnumProperty = DependencyProperty.Register(
+            "Enum", typeof(string), typeof(EnumValueMapping), new PropertyMetadata(default(string)));
+
+        public string Enum
+        {
+            get { return (string)GetValue(EnumProperty); }
+            set { SetValue(EnumProperty, value); }
+        } 
+        #endregion
+
+        #region Value dependency property
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
+            "Value", typeof(object), typeof(EnumValueMapping), new PropertyMetadata(default(object)));
+
+        public object Value
+        {
+            get { return GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        } 
+        #endregion
+    }
+}
